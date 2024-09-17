fb3021bb43a38b3e186666913ed6cc538d523037
EMİRHAN KARAKOÇ <emirhankarakoc@yahoo.com>
2024-09-17 05:17:36 +0400

    frontend homepage fix

--- frontend-reactjs/src/App.tsx
+++ frontend-reactjs/src/App.tsx
@@ -3,9 +3,11 @@ import Homepage from "./pages/homepage/Homepage";
 import Register from "./pages/register/Register";
 import Login from "./pages/login/Login";
 import Profile from "./pages/profile/Profile";
+import Navigation from "./components/Navigation";
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Navigation />} />
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
       <Route path="/profile" element={<Profile />} />
